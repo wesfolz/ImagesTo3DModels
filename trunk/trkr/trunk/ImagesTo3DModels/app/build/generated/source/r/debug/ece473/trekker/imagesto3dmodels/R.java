9e20a66526a69cc8629611285588485f92d01e72
wesfolz <wesfolz@e8e808b8-a417-46c6-94ba-418a53083474>
2015-02-28 23:50:06 +0000

    R.java needs to be committed as well

--- trunk/trkr/trunk/ImagesTo3DModels/app/build/generated/source/r/debug/ece473/trekker/imagesto3dmodels/R.java
+++ trunk/trkr/trunk/ImagesTo3DModels/app/build/generated/source/r/debug/ece473/trekker/imagesto3dmodels/R.java
@@ -1325,6 +1325,7 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f020030;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020031;
         public static final int abc_textfield_search_material=0x7f020032;
+        public static final int ic_launcher=0x7f020033;
     }
     public static final class id {
         public static final int action_bar=0x7f090034;
@@ -1340,14 +1341,18 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f090031;
         public static final int action_mode_bar_stub=0x7f090030;
         public static final int action_mode_close_button=0x7f090023;
-        public static final int action_settings=0x7f090042;
+        public static final int action_settings=0x7f09004b;
         public static final int activity_chooser_view_content=0x7f090024;
         public static final int always=0x7f090019;
         public static final int any=0x7f090013;
         public static final int back=0x7f090014;
         public static final int beginning=0x7f090016;
+        public static final int camera_view=0x7f090042;
+        public static final int capture_button=0x7f090043;
+        public static final int capture_complete_button=0x7f090044;
         public static final int checkbox=0x7f09002c;
         public static final int collapseActionView=0x7f09001a;
+        public static final int create_model_button=0x7f09004a;
         public static final int decor_content_parent=0x7f090032;
         public static final int default_activity_button=0x7f090027;
         public static final int dialog=0x7f09001e;
@@ -1363,12 +1368,16 @@ containing a value of this type.
         public static final int icon=0x7f090029;
         public static final int ifRoom=0x7f09001b;
         public static final int image=0x7f090026;
+        public static final int initiate_capture_button=0x7f090045;
         public static final int listMode=0x7f090009;
         public static final int list_item=0x7f090028;
         public static final int middle=0x7f090018;
+        public static final int model_name=0x7f090047;
         public static final int never=0x7f09001c;
         public static final int none=0x7f09000e;
         public static final int normal=0x7f09000a;
+        public static final int photo_gallery_linear_layout=0x7f090049;
+        public static final int photo_gallery_scroll_view=0x7f090048;
         public static final int progress_circular=0x7f090005;
         public static final int progress_horizontal=0x7f090006;
         public static final int radio=0x7f09002e;
@@ -1389,6 +1398,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f090007;
         public static final int submit_area=0x7f09003f;
         public static final int tabMode=0x7f09000b;
+        public static final int textView=0x7f090046;
         public static final int title=0x7f09002a;
         public static final int up=0x7f090008;
         public static final int useLogo=0x7f090012;
@@ -1422,11 +1432,15 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f040014;
         public static final int abc_search_view=0x7f040015;
         public static final int abc_simple_dropdown_hint=0x7f040016;
-        public static final int activity_main_menu=0x7f040017;
-        public static final int support_simple_spinner_dropdown_item=0x7f040018;
+        public static final int activity_image_capture=0x7f040017;
+        public static final int activity_main_menu=0x7f040018;
+        public static final int activity_model_photo_gallery=0x7f040019;
+        public static final int support_simple_spinner_dropdown_item=0x7f04001a;
     }
     public static final class menu {
-        public static final int menu_main_menu=0x7f0d0000;
+        public static final int menu_image_capture=0x7f0d0000;
+        public static final int menu_main_menu=0x7f0d0001;
+        public static final int menu_model_photo_gallery=0x7f0d0002;
     }
     public static final class mipmap {
         public static final int ic_launcher=0x7f030000;
@@ -1451,6 +1465,8 @@ containing a value of this type.
         public static final int action_settings=0x7f0b0010;
         public static final int app_name=0x7f0b0011;
         public static final int hello_world=0x7f0b0012;
+        public static final int title_activity_image_capture=0x7f0b0013;
+        public static final int title_activity_model_photo_gallery=0x7f0b0014;
     }
     public static final class style {
         /**  Customize your theme here. 
