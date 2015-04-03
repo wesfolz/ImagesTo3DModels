9ef19af6d1cce738928fb4b2ac65bc7895f9af63
wesfolz <wesfolz@e8e808b8-a417-46c6-94ba-418a53083474>
2015-04-03 19:05:14 +0000

    Moved tests involving openCV calls to the ImageCaptureActivityTest class so that the openCV calls don't cause it to crash.

--- trunk/trkr/trunk/ImagesTo3DModels/app/src/androidTest/java/ece473/trekker/imagesto3dmodels/ImageCaptureActivityTest.java
+++ trunk/trkr/trunk/ImagesTo3DModels/app/src/androidTest/java/ece473/trekker/imagesto3dmodels/ImageCaptureActivityTest.java
@@ -1,12 +1,21 @@
 package ece473.trekker.imagesto3dmodels;
 
-import android.hardware.Camera;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
 import android.test.ViewAsserts;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 
 /**
  * Created by Ryan Hoefferle on 3/29/2015.
@@ -30,15 +39,17 @@ public class ImageCaptureActivityTest extends ActivityInstrumentationTestCase2<I
     {
         final ImageButton captureButton = (ImageButton) activity.findViewById( R.id
                 .capture_button );
-
-        activity.runOnUiThread(new Runnable() {
+        String testImagePath = MainMenuActivity.appDir + "/test/images/";
+/*        activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 captureButton.performClick();
             }
         });
+*/
+        Mat image = Highgui.imread( testImagePath + "noBackground.jpg" );
 
-        assertTrue( true );
+        assertNotNull( image );
     }
 
     public void testCalibrationButton() throws Exception{
@@ -49,6 +60,153 @@ public class ImageCaptureActivityTest extends ActivityInstrumentationTestCase2<I
         TouchUtils.clickView(this, flashButton);
 
         assertTrue( true );
+
+    }
+
+
+    /**
+     * Tests requirement 1.4 to make sure image planes are creating outer edges
+     *
+     * @throws Exception
+     */
+    public void testOuterEdges() throws Exception
+    {
+        String testImagePath = MainMenuActivity.appDir + "/test/images/";
+
+        Mat image = Highgui.imread( testImagePath + "noBackground.jpg" );
+
+        ImagePlane plane = new ImagePlane( image, 0 );
+        plane.setPlane( 0, 0 );
+        plane.findMinEdges();
+
+        assertTrue( plane.topEdge.size() > 0 );
+        assertTrue( plane.bottomEdge.size() > 0 );
+        assertTrue( plane.rightEdge.size() > 0 );
+        assertTrue( plane.leftEdge.size() > 0 );
+    }
+
+
+    /**
+     * Tests requirements 1.6 and 1.7 to make sure vertices and triangle faces are being created
+     *
+     * @throws Exception
+     */
+    public void testVerticesAndFaces() throws Exception
+    {
+        String testImagePath = MainMenuActivity.appDir + "/test/images/";
+        Object3DModel model = new Object3DModel( "test", testImagePath );
+        Mat image = Highgui.imread( testImagePath + "noBackground.jpg" );
+
+        ImagePlane plane = new ImagePlane( image, 0 );
+        plane.setPlane( 0, 0 );
+        plane.findMinEdges();
+        model.triangulateImage2D( image, 0, plane.getPlaneFace(), plane.topEdge,
+                plane.bottomEdge, plane.rightEdge, plane.leftEdge );
+        assertTrue( model.getTriangleFaceArray().size() > 0 );
+        assertTrue( model.getVertexArray().size() > 0 );
+    }
+
+
+    /**
+     * Tests requirement 1.8 and 2.3 to see if an output file(.obj) can be saved to the device
+     *
+     * @throws Exception
+     */
+    public void testFileOutput_OBJ() throws Exception
+    {
+
+        String testImagePath = MainMenuActivity.appDir + "/test/images/";
+        Object3DModel model = new Object3DModel( "test", testImagePath );
+        Mat image = Highgui.imread( testImagePath + "noBackground.jpg" );
+
+        ImagePlane plane = new ImagePlane( image, 0 );
+        plane.setPlane( 0, 0 );
+        plane.findMinEdges();
+        model.triangulateImage2D( image, 0, plane.getPlaneFace(), plane.topEdge,
+                plane.bottomEdge, plane.rightEdge, plane.leftEdge );
+
+        model.writeOBJFile( MainMenuActivity.appDir + "/test/test.obj" );
+        File objFile = new File( MainMenuActivity.appDir + "/test/test.obj" );
+        assertTrue( objFile.exists() );
+        assertTrue( objFile.length() > 0 );
+    }
+
+
+    /**
+     * Tests requirement 2.4 to see if a .ply file can be saved to the device
+     *
+     * @throws Exception
+     */
+    public void testFileOutput_PLY() throws Exception
+    {
+        String testImagePath = MainMenuActivity.appDir + "/test/images/";
+        Object3DModel model = new Object3DModel( "test", testImagePath );
+        Mat image = Highgui.imread( testImagePath + "noBackground.jpg" );
+
+        ImagePlane plane = new ImagePlane( image, 0 );
+        plane.setPlane( 0, 0 );
+        plane.findMinEdges();
+        model.triangulateImage2D( image, 0, plane.getPlaneFace(), plane.topEdge,
+                plane.bottomEdge, plane.rightEdge, plane.leftEdge );
+
+        model.writePLYFile( MainMenuActivity.appDir + "/test/test.ply" );
+        File plyFile = new File( MainMenuActivity.appDir + "/test/test.ply" );
+        assertTrue( plyFile.exists() );
+        assertTrue( plyFile.length() > 0 );
+    }
+
+    /**
+     * Tests requirement 1.2 to test if multiple images are saved as internal data structures
+     *
+     * @throws Exception
+     */
+    public void testForMultipleImages() throws Exception
+    {
+
+        OutputStream out = null;
+        String testImagePath = MainMenuActivity.appDir + "/test12/images";
+
+        File testDir = new File( testImagePath );
+        if( testDir.exists() )
+        {
+            MainMenuActivity.DeleteRecursive( testDir );
+        }
+
+        MainMenuActivity.createDirectory( "test12" );
+        MainMenuActivity.createDirectory( "test12/images" );
+
+        View dummyView = activity.findViewById( R.id.create_model_button );
+        Bitmap bitmap = BitmapFactory.decodeResource( MyApplication.getAppContext().getResources
+                (), R.drawable.plus );
+
+        out = new BufferedOutputStream( new FileOutputStream( testImagePath + "/capturetest1.jpg"
+        ) );
+        bitmap.compress( Bitmap.CompressFormat.JPEG, 100, out );
+        out = new BufferedOutputStream( new FileOutputStream( testImagePath + "/capturetest2.jpg"
+        ) );
+        bitmap.compress( Bitmap.CompressFormat.JPEG, 100, out );
+        out = new BufferedOutputStream( new FileOutputStream( testImagePath + "/capturetest3.jpg"
+        ) );
+        bitmap.compress( Bitmap.CompressFormat.JPEG, 100, out );
+        out = new BufferedOutputStream( new FileOutputStream( testImagePath + "/capturetest4.jpg"
+        ) );
+        bitmap.compress( Bitmap.CompressFormat.JPEG, 100, out );
+        out = new BufferedOutputStream( new FileOutputStream( testImagePath + "/capturetest5.jpg"
+        ) );
+        bitmap.compress( Bitmap.CompressFormat.JPEG, 100, out );
+        out = new BufferedOutputStream( new FileOutputStream( testImagePath + "/capturetest6.jpg"
+        ) );
+        bitmap.compress( Bitmap.CompressFormat.JPEG, 100, out );
+
+        File[] images = new File( testImagePath ).listFiles();
+        ArrayList<Mat> imageArray = new ArrayList<>( images.length );
+        //create array list of Mat objects for processing
+        for( File f : images )
+        {
+            imageArray.add( Highgui.imread( f.getAbsolutePath() ) );
+        }
+
+        assertNotNull( imageArray );
 
     }
 
--- trunk/trkr/trunk/ImagesTo3DModels/app/src/androidTest/java/ece473/trekker/imagesto3dmodels/ModelPhotoGalleryActivityTest.java
+++ trunk/trkr/trunk/ImagesTo3DModels/app/src/androidTest/java/ece473/trekker/imagesto3dmodels/ModelPhotoGalleryActivityTest.java
@@ -4,132 +4,39 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.test.ActivityInstrumentationTestCase2;
-import android.util.Log;
 import android.view.View;
-
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Mat;
-import org.opencv.highgui.Highgui;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
 
 /**
  * Created by Ryan Hoefferle on 3/29/2015.
  */
 public class ModelPhotoGalleryActivityTest extends
-        ActivityInstrumentationTestCase2<ModelPhotoGalleryActivity> {
+        ActivityInstrumentationTestCase2<ModelPhotoGalleryActivity>
+{
 
     private ModelPhotoGalleryActivity activity;
 
-    private BaseLoaderCallback mLoaderCallback;
-
-    public ModelPhotoGalleryActivityTest() {
-        super(ModelPhotoGalleryActivity.class);
+    public ModelPhotoGalleryActivityTest()
+    {
+        super( ModelPhotoGalleryActivity.class );
     }
 
     @Override
-    public void setUp() throws Exception {
+    public void setUp() throws Exception
+    {
         super.setUp();
-        setActivityInitialTouchMode(false);
+        setActivityInitialTouchMode( false );
         Intent galleryIntent = new Intent();
-        galleryIntent.setClassName("ece473.trekker.imagesto3dmodels",
-                "ece473.trekker.imagesto3dmodels.ModelPhotoGalleryActivity");
-        galleryIntent.putExtra("modelName", "test");
-        galleryIntent.putExtra("modelImageDirectory", MainMenuActivity.appDir + "/test/images");
-        setActivityIntent(galleryIntent);
+        galleryIntent.setClassName( "ece473.trekker.imagesto3dmodels",
+                "ece473.trekker.imagesto3dmodels.ModelPhotoGalleryActivity" );
+        galleryIntent.putExtra( "modelName", "test" );
+        galleryIntent.putExtra( "modelImageDirectory", MainMenuActivity.appDir + "/test/images" );
+        setActivityIntent( galleryIntent );
         activity = getActivity();
-
-        mLoaderCallback = new BaseLoaderCallback(activity.getApplicationContext()) {
-
-            @Override
-            public void onManagerConnected(int status) {
-                switch (status) {
-                    case LoaderCallbackInterface.SUCCESS: {
-                        synchronized (this) {
-                            notify();
-                        }
-                    }
-                    break;
-                    default: {
-                        super.onManagerConnected(status);
-                    }
-                    break;
-                }
-            }
-        };
-
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, activity.getApplicationContext
-                (), mLoaderCallback);
-
-
-    }
-
-    /**
-     * Tests requirement 1.4 to make sure image planes are creating outer edges
-     *
-     * @throws Exception
-     */
-    public void testOuterEdges() throws Exception {
-        String testImagePath = MainMenuActivity.appDir + "/test/images";
-        Object3DModel model = new Object3DModel("test", testImagePath);
-        Mat image = Highgui.imread(testImagePath + "noBackground.jpg");
-
-        ImagePlane plane = new ImagePlane(image, 0);
-        plane.setPlane(0, 0);
-        plane.findMinEdges();
-
-        assertTrue(plane.topEdge.size() > 0);
-        assertTrue(plane.bottomEdge.size() > 0);
-        assertTrue(plane.rightEdge.size() > 0);
-        assertTrue(plane.leftEdge.size() > 0);
-    }
-
-
-    /**
-     * Tests requirements 1.6 and 1.7 to make sure vertices and triangle faces are being created
-     *
-     * @throws Exception
-     */
-    public void testVerticesAndFaces() throws Exception {
-        String testImagePath = MainMenuActivity.appDir + "/test/images";
-        Object3DModel model = new Object3DModel("test", testImagePath);
-        Mat image = Highgui.imread(testImagePath + "noBackground.jpg");
-
-        ImagePlane plane = new ImagePlane(image, 0);
-        plane.setPlane(0, 0);
-        plane.findMinEdges();
-        model.triangulateImage2D(image, 0, plane.getPlaneFace(), plane.topEdge,
-                plane.bottomEdge, plane.rightEdge, plane.leftEdge);
-        assertTrue(model.getTriangleFaceArray().size() > 0);
-        assertTrue(model.getVertexArray().size() > 0);
-    }
-
-    /**
-     * Tests requirement 1.8 to see if an output file can be saved to the device
-     *
-     * @throws Exception
-     */
-    public void testFileOutput_OBJ() throws Exception {
-        String testImagePath = MainMenuActivity.appDir + "/test/images";
-        Object3DModel model = new Object3DModel("test", testImagePath);
-        Mat image = Highgui.imread(testImagePath + "noBackground.jpg");
-
-        ImagePlane plane = new ImagePlane(image, 0);
-        plane.setPlane(0, 0);
-        plane.findMinEdges();
-        model.triangulateImage2D(image, 0, plane.getPlaneFace(), plane.topEdge,
-                plane.bottomEdge, plane.rightEdge, plane.leftEdge);
-
-        model.writeOBJFile(MainMenuActivity.appDir + "/test");
-        File objFile = new File(MainMenuActivity.appDir + "/test/test.obj");
-        assertTrue(objFile.exists());
-        assertTrue(objFile.length() > 0);
     }
 
     /**
@@ -137,148 +44,123 @@ public class ModelPhotoGalleryActivityTest extends
      *
      * @throws Exception
      */
-    public void testNumberOfImages() throws Exception {
+    public void testNumberOfImages() throws Exception
+    {
 
         OutputStream out = null;
 
         String testImagePath = MainMenuActivity.appDir + "/test/images";
 
-        File testDir = new File(testImagePath);
-        if(testDir.exists()) {
-            MainMenuActivity.DeleteRecursive(testDir);
-        }
-
-        MainMenuActivity.createDirectory("test");
-        MainMenuActivity.createDirectory("test/images");
-
-
-
-        View dummyView = activity.findViewById(R.id.create_model_button);
-        Bitmap bitmap = BitmapFactory.decodeResource(MyApplication.getAppContext().getResources(), R.drawable.plus);
-
-        out = new BufferedOutputStream(new FileOutputStream(testImagePath + "/capturetest1.jpg"));
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-
-                activity.updateImgAdapter();
-
-            }
-        });
-        assertFalse(activity.createModel(dummyView));
-
-        out = new BufferedOutputStream(new FileOutputStream(testImagePath + "/capturetest2.jpg"));
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-
-                activity.updateImgAdapter();
-
-            }
-        });
-        assertFalse(activity.createModel(dummyView));
-
-        out = new BufferedOutputStream(new FileOutputStream(testImagePath + "/capturetest3.jpg"));
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-
-                activity.updateImgAdapter();
-
-            }
-        });
-        assertFalse(activity.createModel(dummyView));
-
-        out = new BufferedOutputStream(new FileOutputStream(testImagePath + "/capturetest4.jpg"));
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-
-                activity.updateImgAdapter();
-
-            }
-        });
-        assertFalse(activity.createModel(dummyView));
-
-        out = new BufferedOutputStream(new FileOutputStream(testImagePath + "/capturetest5.jpg"));
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-
-                activity.updateImgAdapter();
-
-            }
-        });
-        assertFalse(activity.createModel(dummyView));
-
-        out = new BufferedOutputStream(new FileOutputStream(testImagePath + "/capturetest6.jpg"));
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-
-                activity.updateImgAdapter();
-
-            }
-        });
-        assertTrue(activity.createModel(dummyView));
-
-    }
-
-    /**
-     * Tests requirement 1.2 to test if multiple images are saved as internal data structures
-     *
-     * @throws Exception
-     */
-    public void testForMultipleImages() throws Exception {
-
-        OutputStream out = null;
-        String testImagePath = MainMenuActivity.appDir + "/test/images";
-
-        File testDir = new File(testImagePath);
-        if(testDir.exists()) {
-            MainMenuActivity.DeleteRecursive(testDir);
-        }
-
-        MainMenuActivity.createDirectory("test");
-        MainMenuActivity.createDirectory("test/images");
-
-        View dummyView = activity.findViewById(R.id.create_model_button);
-        Bitmap bitmap = BitmapFactory.decodeResource(MyApplication.getAppContext().getResources(), R.drawable.plus);
-
-        out = new BufferedOutputStream(new FileOutputStream(testImagePath + "/capturetest1.jpg"));
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-        out = new BufferedOutputStream(new FileOutputStream(testImagePath + "/capturetest2.jpg"));
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-        out = new BufferedOutputStream(new FileOutputStream(testImagePath + "/capturetest3.jpg"));
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-        out = new BufferedOutputStream(new FileOutputStream(testImagePath + "/capturetest4.jpg"));
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-        out = new BufferedOutputStream(new FileOutputStream(testImagePath + "/capturetest5.jpg"));
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-        out = new BufferedOutputStream(new FileOutputStream(testImagePath + "/capturetest6.jpg"));
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-
-        File[] images = new File( testImagePath ).listFiles();
-        ArrayList<Mat> imageArray = new ArrayList<>( images.length );
-        //create array list of Mat objects for processing
-        for( File f : images )
+        File testDir = new File( testImagePath );
+        if( testDir.exists() )
         {
-            imageArray.add( Highgui.imread( f.getAbsolutePath() ) );
+            MainMenuActivity.DeleteRecursive( testDir );
         }
 
-        assertNotNull(imageArray);
+        MainMenuActivity.createDirectory( "test" );
+        MainMenuActivity.createDirectory( "test/images" );
+
+
+        View dummyView = activity.findViewById( R.id.create_model_button );
+        Bitmap bitmap = BitmapFactory.decodeResource( MyApplication.getAppContext().getResources
+                (), R.drawable.plus );
+
+        out = new BufferedOutputStream( new FileOutputStream( testImagePath + "/capturetest1.jpg"
+        ) );
+        bitmap.compress( Bitmap.CompressFormat.JPEG, 100, out );
+
+        activity.runOnUiThread( new Runnable()
+        {
+            @Override
+            public void run()
+            {
+
+                activity.updateImgAdapter();
+
+            }
+        } );
+        assertFalse( activity.createModel( dummyView ) );
+
+        out = new BufferedOutputStream( new FileOutputStream( testImagePath + "/capturetest2.jpg"
+        ) );
+        bitmap.compress( Bitmap.CompressFormat.JPEG, 100, out );
+        activity.runOnUiThread( new Runnable()
+        {
+            @Override
+            public void run()
+            {
+
+                activity.updateImgAdapter();
+
+            }
+        } );
+        assertFalse( activity.createModel( dummyView ) );
+
+        out = new BufferedOutputStream( new FileOutputStream( testImagePath + "/capturetest3.jpg"
+        ) );
+        bitmap.compress( Bitmap.CompressFormat.JPEG, 100, out );
+        activity.runOnUiThread( new Runnable()
+        {
+            @Override
+            public void run()
+            {
+
+                activity.updateImgAdapter();
+
+            }
+        } );
+        assertFalse( activity.createModel( dummyView ) );
+
+        out = new BufferedOutputStream( new FileOutputStream( testImagePath + "/capturetest4.jpg"
+        ) );
+        bitmap.compress( Bitmap.CompressFormat.JPEG, 100, out );
+        activity.runOnUiThread( new Runnable()
+        {
+            @Override
+            public void run()
+            {
+
+                activity.updateImgAdapter();
+
+            }
+        } );
+        assertFalse( activity.createModel( dummyView ) );
+
+        out = new BufferedOutputStream( new FileOutputStream( testImagePath + "/capturetest5.jpg"
+        ) );
+        bitmap.compress( Bitmap.CompressFormat.JPEG, 100, out );
+        activity.runOnUiThread( new Runnable()
+        {
+            @Override
+            public void run()
+            {
+
+                activity.updateImgAdapter();
+
+            }
+        } );
+        assertFalse( activity.createModel( dummyView ) );
+
+        out = new BufferedOutputStream( new FileOutputStream( testImagePath + "/capturetest6.jpg"
+        ) );
+        bitmap.compress( Bitmap.CompressFormat.JPEG, 100, out );
+        activity.runOnUiThread( new Runnable()
+        {
+            @Override
+            public void run()
+            {
+
+                activity.updateImgAdapter();
+
+            }
+        } );
+        assertTrue( activity.createModel( dummyView ) );
 
     }
 
     @Override
-    public void tearDown() throws Exception {
+    public void tearDown() throws Exception
+    {
         super.tearDown();
     }
 }
